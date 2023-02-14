93c4f5bdbddc38a20cc3eaef6e629ff757c16ad4
Leeseojune <sung07288346@gmail.com>
2023-02-15 02:09:24 +0900

    Initialize Project

---
+++ src/main/kotlin/io/github/e1i2/E1I2ServerApplication.kt
@@ -0,0 +1,11 @@
+package io.github.e1i2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class E1I2ServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<E1I2ServerApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/e1i2/E1I2ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.e1i2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class E1I2ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
