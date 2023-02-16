196aa5a551b39c2a0237d2598568e69314ba7226
jhhong0509 <jhhongb0509@gmail.com>
2023-02-16 22:55:44 +0900

    feat: User Entity Repository 추가

---
+++ src/main/kotlin/io/github/e1i2/user/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package io.github.e1i2.user.repository
+
+import io.github.e1i2.user.User
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : CoroutineCrudRepository<User, Long>
